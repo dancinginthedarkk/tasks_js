dd9110fe39c5db41a2368022ea30b755994adfca
dancinginthedarkk <m.angellina@yandex.ru>
2023-02-18 20:27:18 +0300

    fixed function customReduce

--- 7_customReduce.js
+++ 7_customReduce.js
@@ -1,14 +1,19 @@
 // Напишите аналог встроенного метода reduce для работы с массивами
 
 function customReduce(array, callback, initialValue) {
+    let i = 0;
+    if (initialValue === undefined) {
+        initialValue = array[0];
+        i = 1;
+    }
     let accumulator = initialValue;
-    for (let i = 0; i < array.length; i++) {
+    for (i; i < array.length; i++) {
         accumulator = callback(accumulator, array[i], i, array);
     }
     return accumulator;
 }
 
 const numbers = [1, 2, 3, 4, 5];
-const sum = customReduce(numbers, (accumulator, number) => accumulator + number, 0);
+const sum = customReduce(numbers, (accumulator, number) => accumulator + number);
 
 console.log(sum);
